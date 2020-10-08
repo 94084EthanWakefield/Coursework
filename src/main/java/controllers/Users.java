43ebceb11e24ee939c572228c9e465d615014ce9
94084EthanWakefield <94084@farnborough.ac.uk>
2020-10-08 12:00:33 +0100

    Started doing API calls and restructured files.

---
+++ src/main/java/controllers/Users.java
@@ -0,0 +1,84 @@
+package controllers;
+
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import server.Main;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Path("users/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class Users{
+    @GET
+    @Path("list")
+    public String UsersList() {
+        System.out.println("Invoked Users.UsersList()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT Username, Password FROM Users");
+            ResultSet results = ps.executeQuery();
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("UserID", results.getInt(1));
+                row.put("UserName", results.getString(2));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+
+    @GET
+    @Path("get/{Username}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String GetUser(@PathParam("Username") String Username) {
+        System.out.println("Invoked Users.GetUser() with UserID " + Username);
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserName, Password FROM Users WHERE Username = ?");
+            ps.setString(1, Username);
+            ResultSet results = ps.executeQuery();
+            JSONObject response = new JSONObject();
+            if (results.next()== true) {
+                response.put("UserID", Username);
+                response.put("UserName", results.getString(1));
+                response.put("Token", results.getInt(2));
+            } else {
+                return "{\"Error\": \"No such username found\"}";
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
+    @Path("new")
+    public String UsersAdd(@FormDataParam("Username") String Username, @FormDataParam("Password") String Password, @FormDataParam("Creationdate") String Creationdate) {
+        System.out.println("Invoked Users.UsersAdd()");
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (Username, Password, CreationDate) VALUES (?, ?, ?)");
+            ps.setString(1, Username);
+            ps.setString(2, Password);
+            ps.setString(3, Creationdate);
+            ps.execute();
+            return "{\"OK\": \"Added user.\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+
+    }
+
+
+}
+
